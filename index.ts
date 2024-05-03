c340d0f8715b7cac2607f9cd682005c6a893e608
Muhammad Ali <proqualitythings100@gmail.com>
2024-05-03 21:38:54 +0500

    scoring and looping

--- index.ts
+++ index.ts
@@ -3,32 +3,55 @@
 import inquirer from "inquirer"
 
 let getComputerChoice = () => {
-    const options = ["rock", "paper", "scissors"]
+    const options = ["Rock", "Paper", "Scissors"]
     const randomIndex = Math.floor(Math.random() * 3)
     return options[randomIndex]
 }
 const compChoice = getComputerChoice()
 
-let askUser = await inquirer.prompt([
-    {
-        name: "userChoice",
-        message: "Choose any one of them:",
-        type: "list",
-        choices: ["rock", "paper", "scissors"]
+let userScore = 0
+let compScore = 0
+let tie = 0
+
+let coding = true
+while(coding) {
+
+    let getComputerChoice = () => {
+        const options = ["Rock", "Paper", "Scissors"]
+        const randomIndex = Math.floor(Math.random() * 3)
+        return options[randomIndex]
     }
-])
+    const compChoice = getComputerChoice()
 
-console.log(`Computer chose: ${compChoice}`)
-console.log(`You chose: ${askUser.userChoice}`)
-
-if (compChoice == "rock" && askUser.userChoice == "paper") {
-    console.log("You Won")
-} else if (compChoice == "paper" && askUser.userChoice == "scissors") {
-    console.log("You Won")
-} else if (compChoice == "scissors" && askUser.userChoice == "rock") {
-    console.log("You Won")
-} else if (compChoice == askUser.userChoice) {
-    console.log("It's tie")
-} else {
-    console.log("Computer Win! You lose...")
+    let askUser = await inquirer.prompt([
+        {
+            name: "userChoice",
+            message: "Choose any one of them:",
+            type: "list",
+            choices: ["Rock", "Paper", "Scissors", "Exit"]
+        }
+    ])
+    
+    console.log(`Computer chose: ${compChoice}`)
+    console.log(`You chose: ${askUser.userChoice}`)
+    
+    if (compChoice == "Rock" && askUser.userChoice == "Paper") {
+        console.log("You Won! Computer lose...")
+        userScore++
+    } else if (compChoice == "Paper" && askUser.userChoice == "Scissors") {
+        console.log("You Won! Computer lose...")
+        userScore++
+    } else if (compChoice == "Scissors" && askUser.userChoice == "Rock") {
+        console.log("You Won! Computer lose...")
+        userScore++
+    } else if (compChoice == askUser.userChoice) {
+        console.log("It's tie")
+        tie++
+    } else if (askUser.userChoice == "Exit") {
+        console.log(`Your Score: ${userScore} \nComputer Score: ${compScore} \nTie: ${tie}`)
+        coding = false
+    } else {
+        console.log("Computer Won! You lose...")
+        compScore++
+    }
 }
